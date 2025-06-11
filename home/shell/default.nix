8789dcec385c805c0a213864b7f81fe0ffa3fd4d
Sir Wrexes <ludofernandez@msn.com>
2025-06-11 17:09:23 +0200

    home.fish: Add interactive hell init

--- home/shell/default.nix
+++ home/shell/default.nix
@@ -8,5 +8,16 @@
   programs.fish = {
     plugins = import ./plugins;
     generateCompletions = true;
+    interactiveShellInit =
+      let
+        inherit (pkgs.lib.meta) getExe;
+      in
+      ''
+        fish_vi_key_bindings
+
+        set fish_greeting "Today is a good day to code avec le cul !"
+
+        ${getExe pkgs.starship} init fish | source
+      '';
   };
 }
