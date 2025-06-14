e649c0e9bbe5f8ccfebfe80dbfbd4f8651f27063
Sir Wrexes <ludofernandez@msn.com>
2025-06-14 12:29:14 +0200

    home.neovim: Add completion plugin

--- home/neovim/plugins/code/completion.nix
+++ home/neovim/plugins/code/completion.nix
@@ -6,27 +6,41 @@
 }:
 
 let
-  inherit (lib.types) mkLuaInline;
+  inherit (lib.generators) mkLuaInline toLua;
 
-  toLua = lib.generators.toLua { };
+  # TODO: Find a way to make this less verbose
+  lua = toLua { multiline = config.programs.neovim.lazy-nvim.luaMultiline; };
 in
 {
-  programs.neovim.lazy-nvim.plugins = [
+  # Since I'm using the lazy-nvim module and managing plugins with
+  # NixPkgs, Lazy sees the plugin has a `dir` and skips the build phase.
+  # Thus, we need to install Coq's dependencies manually. It's a
+  # tradeoff for having confs written entirely in Nix, but I'll take it.
+  programs.neovim.extraPython3Packages =
+    pypkgs: with pypkgs; [
+      pynvim-pp
+      pyyaml
+      std2
+    ];
+
+  programs.neovim.lazy-nvim.plugins = with pkgs.vimPlugins; [
     {
       name = "Coq";
-      package = pkgs.vimPlugins.coq_nvim;
-
-      build = ":COQdeps";
+      package = coq_nvim;
+      dependencies = [
+        { package = coq-artifacts; }
+        { package = coq-thirdparty; }
+      ];
 
       event = "InsertEnter";
 
       init = mkLuaInline ''
         function()
           vim.g.coq_settings = ${
-            toLua {
-              auto_start = "shut_up";
+            lua {
+              auto_start = "shut-up";
               keymap = {
-                jump_to_mark = "<Tab>";
+                jump_to_mark = "<C-l>";
               };
             }
           }
