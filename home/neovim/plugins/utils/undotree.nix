eb965381e60efa06d15e21c7eb464052017c052c
Sir Wrexes <ludofernandez@msn.com>
2025-06-20 03:28:23 +0200

    home.neovim: Revert undotree fuckup

--- home/neovim/plugins/utils/undotree.nix
+++ home/neovim/plugins/utils/undotree.nix
@@ -1,13 +1,31 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
+let
+  inherit (lib.generators) mkLuaInline;
+in
 {
   programs.neovim.lazy-nvim.plugins = with pkgs.vimPlugins; [
     {
       package = undotree;
 
       init = mkLuaInline ''
-        vim.opt.undofile = true
-        vim.g.undotree_WindowLayout = 4
+        function()
+          local undocache = vim.fn.stdpath "config" .. "/.undotree"
+
+          if not vim.loop.fs_stat(undocache) then
+            vim.notify("Creating undotree cache at [" .. undocache .. "]")
+            vim.fn.mkdir(undocache, "p", tonumber("777", 8))
+          else
+            assert(
+              vim.fn.isdirectory(undocache),
+              "Cache path " .. undocache .. " is not a directory!"
+            )
+          end
+
+          vim.opt.undodir = undocache
+          vim.opt.undofile = true
+          vim.g.undotree_WindowLayout = 4
+        end
       '';
 
       keys = [
