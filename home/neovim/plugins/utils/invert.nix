2759559362dc0a83e42953932300dff2d2f4bac6
Sir Wrexes <ludofernandez@msn.com>
2025-06-30 10:48:14 +0200

    home.neovim.invert: Fix config for v0.1

--- home/neovim/plugins/utils/invert.nix
+++ home/neovim/plugins/utils/invert.nix
@@ -1,13 +1,33 @@
-{ ... }:
+{ lib, ... }:
 
+let
+  inherit (lib.generators) mkLuaInline;
+in
 {
   programs.neovim.lazy-nvim.plugins = [
     {
       dir = "~/Repos/neovim/invert.nvim";
 
-      opts.custom_inverses = {
-        based = "cringe";
-      };
+      keys =
+      let
+        mkKeys = map (
+          { desc, rhs, ...}@mapping: mapping // {
+            desc = "Invert: ${desc}";
+            rhs = mkLuaInline 
+              # lua
+              ''
+                function() require('invert').${rhs} end
+              '';
+          }
+        );
+      in
+      [
+        {
+          lhs = "<leader>ii";
+          rhs = "invert_at_cursor()";
+          desc = "Invert: token under cursor";
+        }
+      ];
     }
   ];
 }
