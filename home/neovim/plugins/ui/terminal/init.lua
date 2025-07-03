c10b9d2a76ec0a29f059f956892d1667ea69c337
Sir Wrexes <ludofernandez@msn.com>
2025-07-03 12:49:01 +0200

    home.neovim.toggleterm: Use `vim.wo` to set the winbar

--- home/neovim/plugins/ui/terminal/init.lua
+++ home/neovim/plugins/ui/terminal/init.lua
@@ -1,13 +1,13 @@
 function()
     -- Prevent terminals from getting discarded when put in the background
-    vim.o.hidden = true 
+    vim.o.hidden = true
 
     vim.api.nvim_create_autocmd("TermOpen", {
       desc = "Set Terminal mode keymaps",
       pattern = "term://*#toggleterm#*",
       group = vim.api.nvim_create_augroup("ToggleTermInit", { clear = true }),
       callback = function()
-        vim.opt_local.winbar = "%= ::" .. vim.b.toggle_number .. "%="
+        vim.wo.winbar = "%= ::" .. vim.b.toggle_number .. "%="
 
         -- Require a double tap on <esc> to leave terminal mode.
         -- That way, TUIs that rely on <esc> to quit won't make you close the terminal
