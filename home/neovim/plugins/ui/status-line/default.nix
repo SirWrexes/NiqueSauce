460781f27534a79086db9ab81937aa46046b425c
Sir Wrexes <ludofernandez@msn.com>
2025-07-05 06:22:10 +0200

    home.neovim.tab-bar: Drop barbar in favor or bufferline

--- home/neovim/plugins/ui/status-line/default.nix
+++ home/neovim/plugins/ui/status-line/default.nix
@@ -1,9 +1,41 @@
-{ pkgs, lib, ... }:
-
 {
-  programs.neovim.extraLuaConfig = ''
+  config,
+  pkgs,
+  pkgs',
+  lib,
+  ...
+}:
 
-    ${lib.strings.readFile ./config.lua}
+let
+  inherit (lib.generators) mkLuaInline;
+  inherit (config.programs.neovim.lazy-nvim) toLua;
+in
+{
+  programs.neovim.lazy-nvim.plugins = with pkgs.vimPlugins; [
+    {
+      package = lualine-nvim;
 
-  '';
+      dependencies = [ { package = pkgs'.vimPlugins.noice-nvim; } ];
+
+      event = "VeryLazy";
+
+      init =
+        mkLuaInline
+          # lua
+          ''
+            function()
+              vim.g.lualine_laststatus = vim.o.laststatus
+              if vim.fn.argc(-1) > 0 then
+                -- set an empty statusline till lualine loads
+                vim.o.statusline = " "
+              else
+                -- hide the statusline on the starter page
+                vim.o.laststatus = 0
+              end
+            end
+          '';
+
+      opts = ./options.lua;
+    }
+  ];
 }
---
+++ home/neovim/plugins/ui/status-line/options.lua
@@ -0,0 +1,109 @@
+function()
+  -- PERF: we don't need this lualine require madness 🤷
+  require('lualine_require').require = require
+
+  vim.o.laststatus = vim.g.lualine_laststatus
+
+  local opts = {
+    options = {
+      theme = 'auto',
+      globalstatus = vim.o.laststatus == 3,
+      disabled_filetypes = {
+        statusline = { 'dashboard', 'snacks_dashboard' },
+      },
+    },
+    sections = {
+      lualine_a = { 'mode' },
+
+      lualine_b = {
+        'branch',
+        {
+          'diff',
+          symbols = {
+            added = ' ',
+            modified = ' ',
+            removed = ' ',
+          },
+          source = function()
+            local gitsigns = vim.b.gitsigns_status_dict
+            if gitsigns then
+              return {
+                added = gitsigns.added,
+                modified = gitsigns.changed,
+                removed = gitsigns.removed,
+              }
+            end
+          end,
+        },
+      },
+
+      lualine_c = {
+        {
+          'filetype',
+          icon_only = true,
+          separator = '',
+          padding = { left = 1, right = 0 },
+        },
+      },
+
+      lualine_x = {
+        Snacks.profiler.status(),
+        -- stylua: ignore
+        {
+          function() return require("noice").api.status.command.get() end,
+          cond = function() return package.loaded["noice"] and require("noice").api.status.command.has() end,
+          color = function() return { fg = Snacks.util.color("Statement") } end,
+        },
+        -- stylua: ignore
+        {
+          function() return "  " .. require("dap").status() end,
+          cond = function() return package.loaded["dap"] and require("dap").status() ~= "" end,
+          color = function() return { fg = Snacks.util.color("Debug") } end,
+        },
+        {
+          'diagnostics',
+          symbols = {
+            error = ' ',
+            warn = ' ',
+            info = ' ',
+            hint = ' ',
+          },
+        },
+      },
+
+      lualine_y = {
+        { 'progress', separator = ' ', padding = { left = 1, right = 0 } },
+        { 'location', padding = { left = 0, right = 1 } },
+      },
+
+      lualine_z = {
+        function()
+          return ' ' .. os.date '%R'
+        end,
+      },
+    },
+    extensions = { 'nvim-tree', 'toggleterm', 'trouble', 'man', 'lazy', 'fzf' },
+  }
+
+  -- do not add trouble symbols if aerial is enabled
+  -- And allow it to be overriden for some buffer types (see autocmds)
+  if vim.g.trouble_lualine then
+    local trouble = require 'trouble'
+    local symbols = trouble.statusline {
+      mode = 'symbols',
+      groups = {},
+      title = false,
+      filter = { range = true },
+      format = '{kind_icon}{symbol.name:Normal}',
+      hl_group = 'lualine_c_normal',
+    }
+    table.insert(opts.sections.lualine_c, {
+      symbols and symbols.get,
+      cond = function()
+        return vim.b.trouble_lualine ~= false and symbols.has()
+      end,
+    })
+  end
+
+  return opts
+end
