9a550ddf57c78137fa1b5b63fe311593d5f77254
Sir Wrexes <ludofernandez@msn.com>
2025-07-05 06:22:50 +0200

    home.neovim.noice: Add some noice presets

--- home/neovim/plugins/ui/noice.nix
+++ home/neovim/plugins/ui/noice.nix
@@ -21,48 +21,61 @@ in
 
       event = "VeryLazy";
 
-      opts = { };
+      opts = {
+        presets = {
+          bottom_search = true;
+          lsp_doc_border = true;
+          long_message_to_split = true;
+          command_palette = true;
+        };
+      };
 
       keys =
         let
-          setMode = mode: map (key: key // { mode = mode; });
+          mkScrollKeys = map (
+            key:
+            key
+            // {
+              mode = [
+                "n"
+                "i"
+                "s"
+              ];
+              expr = true;
+            }
+          );
           scrollStep = "4";
         in
-        (setMode
-          [ "n" "i" "s" ]
-          [
-            rec {
-              lhs = "<C-f>";
-              rhs =
-                mkLuaInline
-                  # lua
-                  ''
-                    function()
-                      if not require('noice').scroll(${scrollStep}) then
-                        return ${toLua lhs}
-                      end
+        (mkScrollKeys [
+          rec {
+            lhs = "<C-f>";
+            rhs =
+              mkLuaInline
+                # lua
+                ''
+                  function()
+                    if not require('noice').scroll(${scrollStep}) then
+                      return ${toLua lhs}
                     end
-                  '';
-              desc = "Scroll LSP window up ${scrollStep}";
-              expr = true;
-            }
-            rec {
-              lhs = "<C-b>";
-              rhs =
-                mkLuaInline
-                  # lua
-                  ''
-                    function()
-                      if not require('noice').scroll(-${scrollStep}) then
-                        return ${toLua lhs}
-                      end
+                  end
+                '';
+            desc = "Scroll LSP window up ${scrollStep}";
+          }
+          rec {
+            lhs = "<C-b>";
+            rhs =
+              mkLuaInline
+                # lua
+                ''
+                  function()
+                    if not require('noice').scroll(-${scrollStep}) then
+                      return ${toLua lhs}
                     end
-                  '';
-              desc = "Scroll LSP window down ${scrollStep}";
-              expr = true;
-            }
-          ]
-        )
+                  end
+                '';
+            desc = "Scroll LSP window down ${scrollStep}";
+          }
+        ])
         ++ [
           {
             lhs = "<S-CR>";
