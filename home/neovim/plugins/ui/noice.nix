0795ffff0e03217c62e63403afcc81b77260eb2d
Sir Wrexes <ludofernandez@msn.com>
2025-07-05 05:03:47 +0200

    home.neovim.ui.noice: Use unstable packages

--- home/neovim/plugins/ui/noice.nix
+++ home/neovim/plugins/ui/noice.nix
@@ -1,18 +1,83 @@
-{ pkgs, lib, ... }:
-
 {
-  programs.neovim.lazy-nvim.plugins = with pkgs.vimPlugins; [
+  pkgs,
+  pkgs',
+  lib,
+  ...
+}:
+
+let
+  inherit (lib.generators) mkLuaInline;
+  toLua = lib.generators.toLua { };
+in
+{
+  programs.neovim.lazy-nvim.plugins = with pkgs'.vimPlugins; [
     {
       package = noice-nvim;
 
       dependencies = [
-        { package = nvim-treesitter; }
+        { package = pkgs.vimPlugins.nvim-treesitter; }
         { package = nui-nvim; }
       ];
 
       event = "VeryLazy";
 
       opts = { };
+
+      keys =
+        let
+          setMode = mode: map (key: key // { mode = mode; });
+          scrollStep = "4";
+        in
+        (setMode
+          [ "n" "i" "s" ]
+          [
+            rec {
+              lhs = "<C-f>";
+              rhs =
+                mkLuaInline
+                  # lua
+                  ''
+                    function()
+                      if not require('noice').scroll(${scrollStep}) then
+                        return ${toLua lhs}
+                      end
+                    end
+                  '';
+              desc = "Scroll LSP window up ${scrollStep}";
+              expr = true;
+            }
+            rec {
+              lhs = "<C-b>";
+              rhs =
+                mkLuaInline
+                  # lua
+                  ''
+                    function()
+                      if not require('noice').scroll(-${scrollStep}) then
+                        return ${toLua lhs}
+                      end
+                    end
+                  '';
+              desc = "Scroll LSP window down ${scrollStep}";
+              expr = true;
+            }
+          ]
+        )
+        ++ [
+          {
+            lhs = "<S-CR>";
+            rhs =
+              mkLuaInline
+                # lua
+                ''
+                  function()
+                    require('noice').redirect(vim.fn.getcmdline())
+                  end
+                '';
+            mode = "c";
+            desc = "Redirect commandline";
+          }
+        ];
     }
   ];
 }
