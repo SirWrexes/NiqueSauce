7cd359809e1440a0499907759ccfd4143477f3bf
Sir Wrexes <ludofernandez@msn.com>
2025-07-13 16:09:55 +0200

    home.neovim.lang.go: Add linter, disable `listchars.tab`

--- home/neovim/plugins/code/lang/go/default.nix
+++ home/neovim/plugins/code/lang/go/default.nix
@@ -2,12 +2,36 @@
 
 let
   server = pkgs.gopls;
-  exe = lib.meta.getExe server;
+  exe.server = lib.meta.getExe server;
+
+  linter = pkgs.golangci-lint-langserver;
+  exe.linter = lib.meta.getExe linter;
 in
 {
-  home.packages = [ server ];
+  home.packages = [
+    server
+    linter
+    pkgs.golangci-lint
+  ];
 
-  programs.neovim.lazy-nvim.lspconfig.servers.gopls = {
-    cmd = [ exe ];
+  programs.neovim.lazy-nvim.lspconfig.servers = {
+    gopls.cmd = [ exe.server ];
+    golangci_lint_ls.cmd = [ exe.linter ];
   };
+
+  programs.neovim.extraLuaConfig =
+    # lua
+    ''
+
+      -- Go's standards is filthy disgusting tabs.
+      -- That makes the tab revealer setting I have visually pollute my sources.
+      vim.api.nvim_create_autocmd("FileType", {
+        group = vim.api.nvim_create_augroup("GolangDisableListCharsTab", { clear = true }),
+        pattern = { "go", "gomod" },
+        callback = function()
+          vim.opt_local.listchars:append { tab = "  " }
+        end
+      })
+
+    '';
 }
